a1214382fb81e172893c76d215c51d6fa3fd27de
ummehanisheikh1234 <ummehansheikh091@gmail.com>
2023-07-08 00:42:47 -0700

    work complete

---
+++ index.py
@@ -0,0 +1,65 @@
+# # print("Tabassum Tabarak")
+# # print("hello world")
+# a=2
+# b=3
+# d=a+b
+# print(d)
+
+# x="Tabassum"
+# y="13"  + x
+# print(y)
+
+# f=4
+# f=5
+# print(f)
+
+# x=8*5+5-4/2
+# print(x)
+
+# x= (8*(5+5)-4)/2
+# print(x)
+
+
+# x= 8*5+5-4/2
+# print(x)
+
+# a=3
+# b=10%a
+# print(b)
+age=19
+age +=1
+print(age)
+age*=3
+
+ 
+if age ==30:
+     print("this is correct age")
+else:
+    print("this is not correct")
+
+perc =int(input())
+if perc <=100 and perc >=80:
+     print("this is your grade :A+")
+elif  perc <=80 and perc >=70:
+     print("this is your grade :A") 
+    
+elif  perc <=70 and perc >=65:
+     print("this is your grade :B+")  
+
+elif  perc <=64 and perc >=60:
+     print("this is your grade :B") 
+elif  perc <=59 and perc >=50:
+     print("this is your grade :C+") 
+elif  perc <=49 and perc >=40:
+     print("this is your grade :D+")
+elif  perc <=39 and perc >=30:
+     print("this is your grade :D") 
+elif perc <100 or perc <0:
+     print("this is not valid")       
+else:
+     print("failed")
+
+
+
+
+
